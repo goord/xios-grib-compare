6e4d8ff9856c64ac05bd32a4863f3b7a483ff050
gvdoord <g.vandenOord@gmail.com>
2019-05-13 18:01:02 +0200

    Fixed script multi-file arguments

--- validate.py
+++ validate.py
@@ -10,8 +10,10 @@ def main(args=None):
     if args is None:
         pass
     parser = argparse.ArgumentParser(description="Automatic comparison IFS GRIB and XIOS NetCDF output")
-    parser.add_argument("--grb", required=True, metavar="FILES", type=str, help="IFS output grib files (concatenated)")
-    parser.add_argument("--nc", required=True, metavar="FILES", type=str, help="XIOS output nc files")
+    parser.add_argument("--grb", required=True, metavar="FILES", type=argparse.FileType('r'), nargs='+',
+                        help="IFS output grib files (concatenated)")
+    parser.add_argument("--nc", required=True, metavar="FILES", type=argparse.FileType('r'), nargs='+',
+                        help="XIOS output nc files")
 
     args = parser.parse_args()
 
