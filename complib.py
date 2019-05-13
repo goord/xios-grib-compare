f2fe319411814fab079f9ad3a46e321a8caca70b
goord <G.vandenOord@gmail.com>
2019-05-13 20:52:36 +0000

    Fixed various issues

--- complib.py
+++ complib.py
@@ -8,6 +8,8 @@ shvars = ["pt", "mont", "pres", "etadot", "z", "t", "u", "v", "w", "vo", "d", "r
 
 level_types = {"sfc": [1, 111], "ml": [109], "pl": [100, 210], "pv": [117]}
 
+temp_dir = os.path.join(os.getcwd(), "tmp")
+
 
 def extract_variables(ncfiles):
     result = []
@@ -41,6 +43,11 @@ def compare_vars(nc_files, grib_files, num_threads):
         else:
             grbf = sfgrb
         grbvars.append(v + (grbf,))
+    if os.path.exists(temp_dir):
+        if any(os.path.listdir(temp_dir)):
+            raise Exception("Temporary working directory exists and is not empty")
+    else:
+        os.makedirs(temp_dir)
     pool = multiprocessing.Pool(processes=num_threads)
     pool.map(postproc_worker, grbvars)
 
@@ -50,13 +57,13 @@ def postproc_worker(vartuple):
     file_atts = os.path.basename(ncfile)[:-3].split('_')
     freq, grid_type, lev_type = file_atts[1], file_atts[2], file_atts[3]
     filter_levels = level_types[lev_type]
-    newfile = "_".join([varname, lev_type]) + ".grib"
+    newfile = os.path.join(temp_dir, "_".join([varname, lev_type]) + ".grib")
     with open(grbfile, 'r') as grib_in, open(newfile, 'w') as grib_out:
         while True:
             record = gribapi.grib_new_from_file(grib_in)
             if record is None:
                 break
             if str(gribapi.grib_get_long(record, "shortName")) == varname and \
-                    int(gribapi.grib_get_long(record, "levelType")) in filter_levels:
+                    int(gribapi.grib_get_long(record, "indicatorOfTypeOfLevel")) in filter_levels:
                 gribapi.write(record, grib_out)
-    print "done"
+    print "wrote", newfile
--- validate.py
+++ validate.py
@@ -10,14 +10,14 @@ def main(args=None):
     if args is None:
         pass
     parser = argparse.ArgumentParser(description="Automatic comparison IFS GRIB and XIOS NetCDF output")
-    parser.add_argument("--grb", required=True, metavar="FILES", type=argparse.FileType('r'), nargs='+',
-                        help="IFS output grib files (concatenated)")
-    parser.add_argument("--nc", required=True, metavar="FILES", type=argparse.FileType('r'), nargs='+',
-                        help="XIOS output nc files")
+    parser.add_argument("--grb", required=True, metavar="FILES", nargs='+', help="IFS output grib files (concatenated)")
+    parser.add_argument("--nc", required=True, metavar="FILES", nargs='+', help="XIOS output nc files")
+    parser.add_argument("--tmpdir", metavar="DIR", default="./tmp", help="Temporary directory")
 
     args = parser.parse_args()
 
-    complib.compare_vars(args.nc, args.grb, 1)
+    complib.temp_dir=args.tmpdir
+    complib.compare_vars(args.nc, args.grb, num_threads=1)
 
 
 if __name__ == "__main__":
