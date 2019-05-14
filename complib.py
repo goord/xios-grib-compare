eda5008264f235055ef975164fe4cf70bdf7b479
gvdoord <g.vandenOord@gmail.com>
2019-05-14 17:44:59 +0200

    Added number of threads to args

--- complib.py
+++ complib.py
@@ -1,9 +1,8 @@
-import os
 import multiprocessing
-import resource
-import gribapi
-import cdo
+import os
 
+import cdo
+import gribapi
 import netCDF4
 
 shvars = ["pt", "mont", "pres", "etadot", "z", "t", "u", "v", "w", "vo", "d", "r"]
@@ -32,38 +31,39 @@ def extract_variables(ncfiles):
 def compare_vars(nc_files, grib_files, num_threads):
     ncvars = extract_variables(nc_files)
     tmp_grbs = {}
-#    if os.path.exists(temp_dir):
-#        if any(os.listdir(temp_dir)):
-#            raise Exception("Temporary working directory exists and is not empty")
-#    else:
-#        os.makedirs(temp_dir)
+    if os.path.exists(temp_dir):
+        if any(os.listdir(temp_dir)):
+            raise Exception("Temporary working directory exists and is not empty")
+    else:
+        os.makedirs(temp_dir)
     grbvars = []
     for v in ncvars:
         ncfile = v[1]
         file_atts = os.path.basename(ncfile)[:-3].split('_')
         freq, grid_type, lev_type = file_atts[1], file_atts[2], file_atts[3]
+        if grid_type != "regular":
+            print "Dismissing variables on reduced grid in", ncfile
         key = (v[0], lev_type)
         fpath = os.path.join(temp_dir, "_".join([v[0], lev_type]) + ".grib")
-#        f = open(fpath, 'w')
-#        tmp_grbs[key] = f
+        f = open(fpath, 'w')
+        tmp_grbs[key] = f
         grbvars.append((v[0], v[1], fpath))
-#    for grib_file in grib_files:
-#        print "Splitting input file", grib_file
-#        with open(grib_file, 'r') as grib_in:
-#            while True:
-#                record = gribapi.grib_new_from_file(grib_in)
-#                if record is None:
-#                    break
-#                varname = str(gribapi.grib_get(record, "shortName"))
-#                ltype = level_types.get(str(gribapi.grib_get(record, "typeOfLevel")), "none")
-#                if (varname, ltype) in tmp_grbs:
-#                    gribapi.grib_write(record, tmp_grbs[(varname, ltype)])
-#                gribapi.grib_release(record)
-#    for grb in tmp_grbs.values():
-#        grb.close()
+    for grib_file in grib_files:
+        print "Splitting input file", grib_file
+        with open(grib_file, 'r') as grib_in:
+            while True:
+                record = gribapi.grib_new_from_file(grib_in)
+                if record is None:
+                    break
+                varname = str(gribapi.grib_get(record, "shortName"))
+                ltype = level_types.get(str(gribapi.grib_get(record, "typeOfLevel")), "none")
+                if (varname, ltype) in tmp_grbs:
+                    gribapi.grib_write(record, tmp_grbs[(varname, ltype)])
+                gribapi.grib_release(record)
+    for grb in tmp_grbs.values():
+        grb.close()
     pool = multiprocessing.Pool(processes=num_threads)
     pool.map(postproc_worker, grbvars)
-
 
 
 def postproc_worker(vartuple):
@@ -72,7 +72,20 @@ def postproc_worker(vartuple):
     freq, grid_type, lev_type = file_atts[1], file_atts[2], file_atts[3]
     app = cdo.Cdo()
     print "Processing", grbfile
-    if varname in shvars:
-        app.sp2gpl(input=grbfile, output=grbfile.replace(".grib", ".nc"), options="-f nc")
+    freqopt = None
+    if freq == "3h":
+        freqopt = "-selhour,0,3,6,9,12,15,18,21"
+    elif freq == "6h":
+        freqopt = "-selhour,0,6,12,18"
+    elif freq == "1d":
+        freqopt = "-daymean"
+    elif freq == "1m":
+        freqopt = "-monmean"
     else:
-        app.copy(input=grbfile, output=grbfile.replace(".grib", ".nc"), options="-f nc -R")
+        print "Frequency", freqopt, "not recognized, skipping variable", varname, "from file", ncfile
+        return
+    output = grbfile.replace(".grib", "_" + freq + ".nc")
+    if varname in shvars:
+        app.sp2gpl(input=grbfile, output=output, options="-f nc " + freqopt)
+    else:
+        app.copy(input=grbfile, output=output, options="-f nc -R " + freqopt)
--- validate.py
+++ validate.py
@@ -6,18 +6,18 @@ import complib
 
 
 def main(args=None):
-
     if args is None:
         pass
     parser = argparse.ArgumentParser(description="Automatic comparison IFS GRIB and XIOS NetCDF output")
     parser.add_argument("--grb", required=True, metavar="FILES", nargs='+', help="IFS output grib files (concatenated)")
     parser.add_argument("--nc", required=True, metavar="FILES", nargs='+', help="XIOS output nc files")
     parser.add_argument("--tmpdir", metavar="DIR", default="./tmp", help="Temporary directory")
+    parser.add_argument("--np", metavar="N", default=1, help="Number of parallel threads")
 
     args = parser.parse_args()
 
-    complib.temp_dir=args.tmpdir
-    complib.compare_vars(args.nc, args.grb, num_threads=1)
+    complib.temp_dir = args.tmpdir
+    complib.compare_vars(args.nc, args.grb, num_threads=int(args.np))
 
 
 if __name__ == "__main__":
