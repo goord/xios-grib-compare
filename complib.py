e9070ff6f34e4147c2d7a4fdcc9521c5d156633d
gvdoord <g.vandenOord@gmail.com>
2021-01-26 22:50:53 +0100

    Added 3d plots

--- complib.py
+++ complib.py
@@ -28,7 +28,7 @@ def get_xios_step(itim, dims):
 
 
 def compare_data(gribfile, ncfile, dims=2):
-    recvars = set(plot_vars + map_vars) if dims == 2 else set(prof_vars)
+    recvars = set(plot_vars + map_vars)
     dsxios = nc.Dataset(ncfile, 'r')
     step, prevstep, itim, pl = 0, -1, 0, None
     errorbar_result = {}
@@ -55,28 +55,41 @@ def compare_data(gribfile, ncfile, dims=2):
                 if refval == 0.:
                     refval = 1
                 nbits = int(gribapi.grib_get(record, "bitsPerValue"))
-                fldxios = dsxios.variables[varname][jtim, ...]
+                if dims == 3:
+                    lev = str(gribapi.grib_get(record, "level"))
+                    fldxios = dsxios.variables[varname][jtim, lev, ...]
+                    key = (varname, lev)
+                else:
+                    lev = -1
+                    fldxios = dsxios.variables[varname][jtim, ...]
+                    key = varname
                 absdiffindex = np.argmax(np.abs(fldgrib - fldxios))
                 absdiff = fldgrib[absdiffindex] - fldxios[absdiffindex]
                 reldiff = absdiff / refval
                 resgrib = (gribapi.grib_get(record, "maximum") - gribapi.grib_get(record, "minimum")) / (2 ** nbits * refval)
-                if varname not in errorbar_result:
-                    errorbar_result[varname] = (absdiff, reldiff, resgrib, step)
+                if key not in errorbar_result:
+                    errorbar_result[key] = (absdiff, reldiff, resgrib, step)
                 else:
-                    a, r, res, stp = errorbar_result[varname]
+                    a, r, res, stp = errorbar_result[key]
                     if abs(absdiff) > abs(a):
-                        errorbar_result[varname] = (absdiff, reldiff, resgrib, step)
-                if varname in map_vars:
-                    if varname not in map_result or abs(absdiff) > abs(errorbar_result[varname][0]):
-                        map_result[varname] = fldgrib - fldxios
+                        errorbar_result[key] = (absdiff, reldiff, resgrib, step)
+                if key in map_vars:
+                    if key not in map_result or abs(absdiff) > abs(errorbar_result[key][0]):
+                        map_result[key] = fldgrib - fldxios
             prevstep = step
             gribapi.grib_release(record)
-    fmt = "{:>20}" * 5
-    log.info(fmt.format("variable", "abs. diff.", "rel. diff.", "grb. res.", "step"))
-    for key, item in errorbar_result.items():
-        log.info(fmt.format(key, *item))
-    plot_error_bars(errorbar_result)
-    plot_error_maps(map_result, dsxios)
+    fmt = "{:>20}" * 6 if dims == 3 else "{:>20}" * 5
+    if dims == 3:
+        log.info(fmt.format("variable", "level", "abs. diff.", "rel. diff.", "grb. res.", "step"))
+        for key, item in errorbar_result.items():
+            log.info(fmt.format(key[0], key[1], *item))
+        plot_error_profs(errorbar_result)
+    else:
+        log.info(fmt.format("variable", "abs. diff.", "rel. diff.", "grb. res.", "step"))
+        for key, item in errorbar_result.items():
+            log.info(fmt.format(key, *item))
+        plot_error_bars(errorbar_result)
+        plot_error_maps(map_result, dsxios)
 
 
 def plot_error_bars(errorbar_result):
@@ -122,5 +135,28 @@ def plot_error_maps(map_result, dsxios):
         ax.coastlines()
         plt.colorbar(fraction=0.036, pad=0.04)  # draw colorbar
         plt.savefig(varname + "_map.png", dpi=300)
+        plt.clf()
+        plt.close()
+
+
+def plot_error_profs(errorbar_result):
+    varnames = set([k[0] for k in errorbar_result.keys()])
+    for varname in varnames:
+        levs = sorted([k[1] for k in errorbar_result.keys() if k[0] == varname])
+        vals = [errorbar_result[(varname, lv)] for lv in levs]
+        values = np.array([v[1] for v in vals])
+        errors = np.array([v[2] for v in vals])
+        plt.style.use("ggplot")
+        markers, caps, bars = plt.errorbar(y=list(range(0, len(values))), x=values, xerr=errors, ecolor="darkred",
+                                           capsize=8, elinewidth=10, ls='none', marker='_', markeredgecolor="black",
+                                           markersize=8)
+        [bar.set_alpha(0.5) for bar in bars]
+        [cap.set_alpha(0.5) for cap in caps]
+        plt.axvline(0, color="black", linestyle=":")
+        plt.xscale("symlog", linthreshy=0.00001)
+        plt.xlabel("relative error")
+#        plt.yticks(list(range(0, len(values))), errorbar_result.keys())
+        #    plt.show()
+        plt.savefig("profplot.png", dpi=300)
         plt.clf()
         plt.close()
--- validate.py
+++ validate.py
@@ -11,11 +11,14 @@ def main(args=None):
     parser = argparse.ArgumentParser(description="Automatic comparison IFS GRIB and XIOS NetCDF output")
     parser.add_argument("--grb", required=True, type=str, metavar="FILE", help="IFS output grib files (concatenated)")
     parser.add_argument("--nc", required=True, type=str, metavar="FILE.nc", help="XIOS output nc files")
-#    parser.add_argument("--var", required=True, type=str, help="variable (short)name")
+    parser.add_argument("--var", required=True, type=str, help="variable (short)name")
 
     args = parser.parse_args()
+    d = 2
+    if args.nc[:-3].endswith("_ml"):
+        d = 3
 
-    complib.compare_data(args.grb, args.nc)
+    complib.compare_data(args.grb, args.nc, d)
 
 
 if __name__ == "__main__":
